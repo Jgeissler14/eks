9d8cf1add03bb085424c6d75f33d4de2bc106954
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-09 18:07:04 -0400

    remove ebs

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -20,6 +20,11 @@ resource "aws_iam_policy" "eks_cluster_policy" {
   policy = file("${path.module}/min-iam.json")
 }
 
+resource "aws_iam_role_policy_attachment" "eks_node_eks_cluster_policy_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = aws_iam_policy.eks_cluster_policy.arn
@@ -55,9 +60,4 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
 resource "aws_iam_role_policy_attachment" "eks_container_registry_policy_attachment" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_eks_cluster_policy_attachment" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
