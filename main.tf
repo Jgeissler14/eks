a9f38f7445fa606f690cda4c54d249036125df6d
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-09 17:55:23 -0400

    remove ebs

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "eks_blueprints_addons" {
     enable_argocd = true
 
     # Add-ons
-    enable_amazon_eks_aws_ebs_csi_driver = true
+    # enable_amazon_eks_aws_ebs_csi_driver = true
     enable_aws_load_balancer_controller  = true
     enable_cert_manager                  = true
     #   enable_karpenter                     = true
