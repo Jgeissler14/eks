f2c06ee02330f4dbda4737c150487a2734202855
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-09 17:07:16 -0400

    kube provider

--- provider.tf
+++ provider.tf
@@ -25,9 +25,5 @@ provider "helm" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+  token                  = module.eks.cluster_token
 }
