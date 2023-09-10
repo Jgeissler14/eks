d05dd1cbc2e68ccf10b815f6604181072f504247
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-09 20:37:15 -0400

    remove argo

--- main.tf
+++ main.tf
@@ -19,36 +19,7 @@ module "eks_blueprints_addons" {
     eks_oidc_provider     = module.eks.oidc_provider
     eks_oidc_provider_arn = module.eks.oidc_provider_arn
 
-    # ArgoCD
-    enable_argocd                       = true
-    argocd_manage_add_ons               = true
-    argocd_admin_password_secret_name   = "admin"
-
-    argocd_helm_config = {
-        name             = "argo-cd"
-        chart            = "argo-cd"
-        repository       = "https://argoproj.github.io/argo-helm"
-        version          = "3.29.5"
-        namespace        = "argocd"
-        timeout          = "1200"
-        create_namespace = true
-        # values = [templatefile("${path.module}/argocd-values.yaml", {})]
-    }
-
-    argocd_applications = {
-    workloads = {
-        path                = "envs/dev"
-        repo_url            = "https://github.com/aws-samples/eks-blueprints-workloads.git"
-        values              = {}
-    }
-    addons = {
-        path                = "chart"
-        repo_url            = "git@github.com:aws-samples/eks-blueprints-add-ons.git"
-        add_on_application  = true              # Indicates the root add-on application.
-        ssh_key_secret_name = "github-ssh-key"  # Needed for private repos
-        values              = {}
-    }
-    }
+    
 
     # Add-ons
     # enable_amazon_eks_aws_ebs_csi_driver = true
@@ -63,7 +34,6 @@ module "eks_blueprints_addons" {
 
     #   enable_karpenter                     = true
     #   enable_metrics_server                = true
-    #   enable_argo_rollouts                 = true
 
     tags = local.tags
 
