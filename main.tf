c430a1169a75b26079de70f8e37077730ede6c1a
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-15 15:49:04 -0400

    name

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ module "eks_blueprints_kubernetes_addons" {
     source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.32.1"
 
 
-    eks_cluster_id               = module.eks.cluster_id
+    eks_cluster_id               = module.eks.cluster_name
     eks_cluster_endpoint         = module.eks.cluster_endpoint
     eks_oidc_provider            = module.eks.oidc_provider
     eks_cluster_version          = module.eks.cluster_platform_version
