3f9b04c7c335be904d39d83e035493100a0b00b3
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-15 17:47:16 -0400

    repo structure

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ module "eks_blueprints_kubernetes_addons" {
     #---------------------------------------------------------------
     argocd_applications = {
       workloads = {
-        path               = "helm/argocd/apps"
+        path               = "helm/argocd/workloads"
         repo_url           = local.repo
         add_on_application = false
         values = {
