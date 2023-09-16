37d2dd211915d2b05e0da1333875a23316644b94
Joshua Geissler <jrgeissler14@gmail.com>
2023-09-16 08:00:33 -0400

    argo repo name

--- cluster_auth.sh
+++ cluster_auth.sh
@@ -2,5 +2,5 @@
 
 EKS_CLUSTER_NAME=$(terraform output -raw cluster_name)
 aws eks --region us-east-1 update-kubeconfig --name $EKS_CLUSTER_NAME
-kubectl config get-contexts
-kubectl config use-context $EKS_CLUSTER_NAME
+AWS_ACCOUNT_NUMBER=$(aws sts get-caller-identity --query Account --output text)
+kubectl config use-context arn:aws:eks:us-east-1:$AWS_ACCOUNT_NUMBER:cluster/$EKS_CLUSTER_NAME
